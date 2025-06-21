381289be0eeb0bd6a55b8aad8b861dd6b9873d69
Ivan Cheban <ivancheban@gmail.com>
2025-06-21 20:04:33 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
 
 import react from '@astrojs/react';
 import stylex from 'astro-stylex';
@@ -9,6 +10,10 @@ import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ua-writers.netlify.app',
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    // If you have rehypePlugins, keep them here
+  },
   integrations: [react(), stylex(), partytown({
     config: {
       forward: ["dataLayer.push"]
